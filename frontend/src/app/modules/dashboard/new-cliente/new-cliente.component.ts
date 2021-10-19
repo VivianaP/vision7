fe4839d3cfa1aa6701c70cee159f0a7197063b0c
Viviana Patiño <nvpatino07@misena.edu.co>
2021-10-18 19:21:48 -0500

    reset 2

---
+++ frontend/src/app/modules/dashboard/new-cliente/new-cliente.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-new-cliente',
+  templateUrl: './new-cliente.component.html',
+  styleUrls: ['./new-cliente.component.scss']
+})
+export class NewClienteComponent implements OnInit {
+  
+
+ 
+
+  constructor() { }
+
+  
+
+  ngOnInit(): void {
+  }
+
+  
+
+}
